a9988f053e2c8f4c21cfe3281d6dfa9cd62e9bee
ArcticRug25 <wyw82839148@163.com>
2023-03-13 15:57:53 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
+
+onLaunch(() => {
+  console.log('App Launch')
+})
+onShow(() => {
+  console.log('App Show')
+})
+onHide(() => {
+  console.log('App Hide')
+})
+</script>
+<style>
+/* #ifdef APP-NVUE */
+@import './tmui/scss/nvue.css';
+/* #endif */
+/* #ifndef APP-NVUE */
+@import './tmui/scss/noNvue.css';
+/* #endif */
+
+/* #ifdef H5 */
+
+body::-webkit-scrollbar,
+div::-webkit-scrollbar,
+*::-webkit-scrollbar {
+  display: none;
+}
+
+body.pages-index-index uni-page-body,
+body {
+  padding-bottom: 0 !important;
+}
+text {
+  font-family: 'sans-serif';
+}
+
+/* #endif */
+</style>
